b17a1ce9c2c700967714b5abbbb1c17100e2d29f
Nick Caruso <ncaruso@gmail.com>
2016-10-31 20:56:09 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# bot-nodejs-demo
+Demo of building a chatbot "bot" with NodeJS
+
+Get a demo token here: https://api.slack.com/docs/oauth-test-tokens
+
+To run type `npm run dev` however, you'll need environment variables for:
+- YouTube
+- API.ai
+- Slack
+
+So you'll probably need to type in `export aiToken=xxx && export slackToken=yyy && export youtubeToken=zzz && npm run dev` where x y and z are appropriate token/keys
---
+++ server.js
@@ -0,0 +1,25 @@
+var restify = require('restify');
+
+var botController = require('./lib/botControllers');
+
+var server = restify.createServer({
+  name: 'bot-nodejs-demo',
+  version: '1.0.0',
+});
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.get('/echo/:name', function (req, res, next) {
+  console.log('received request');
+  res.send(req.params);
+  return next();
+});
+
+var port = process.env.PORT || 8080; // azure will specify port
+
+server.listen(port, function() {
+  botController.start();
+  console.log('%s listening at %s', server.name, server.url);
+});
